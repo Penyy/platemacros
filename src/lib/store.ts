3ff5203cee1e687af42d9f99dc3e3c2efe075da9
stpnk_ <stepienmichauu@gmail.com>
2026-06-12 22:47:22 +0200

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -139,11 +139,13 @@ interface State {
   burned: Record<string, number>;
   products: Product[];
   dayOffs: Set<string>;
+  selectedDate: string;
   // ui
   addSheet: { open: boolean; meal?: Meal; date?: string };
   // ui actions
   openAdd: (meal?: Meal, date?: string) => void;
   closeAdd: () => void;
+  setSelectedDate: (date: string) => void;
   // auth actions
   setAuth: (userId: string | null) => void;
   setOnline: (v: boolean) => void;
@@ -225,10 +227,12 @@ export const usePlate = create<State>()((set, get) => ({
   burned: {},
   products: [],
   dayOffs: new Set<string>(),
+  selectedDate: ymd(new Date()),
   addSheet: { open: false },
 
-  openAdd: (meal, date) => set({ addSheet: { open: true, meal, date } }),
+  openAdd: (meal, date) => set((s) => ({ addSheet: { open: true, meal, date: date ?? s.selectedDate } })),
   closeAdd: () => set((s) => ({ addSheet: { ...s.addSheet, open: false } })),
+  setSelectedDate: (date) => set({ selectedDate: date }),
 
   setAuth: (userId) => {
     set({ userId, authReady: true });
@@ -247,6 +251,7 @@ export const usePlate = create<State>()((set, get) => ({
       burned: {},
       products: [],
       dayOffs: new Set<string>(),
+      selectedDate: ymd(new Date()),
       hydrated: false,
     }),
 
